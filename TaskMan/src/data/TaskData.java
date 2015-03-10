25b43de83a2481f845fa468301d963a187f19932
dries <dries@10.92.36.103>
2015-03-10 14:30:39 +0100

    data object implementaties

--- TaskMan/src/data/TaskData.java
+++ TaskMan/src/data/TaskData.java
@@ -1,5 +1,69 @@
 package data;
 
-public class TaskData {
+import java.util.List;
 
+import task.Task;
+import task.User;
+
+public class TaskData {
+	private String description;
+    private User user;
+    private int estimatedDuration; // in minutes
+    private double acceptableDeviation; // percentage
+    // contains the tasks that need to be completed before this task can start
+    private List<Task> dependencyTasks;
+    // the alternateTask can replace this task to complete the project
+    private Task alternateTask;
+    
+    public String getDescription() {
+    	return description;
+        }
+
+        public void setDescription(String description) throws Exception {
+    	this.description = description;
+        }
+
+        public String getUserName() {
+    	return user.getName();
+        }
+
+        public void setUser(User user) throws Exception {
+    	this.user = user;
+        }
+
+        public int getEstimatedDuration() {
+    	return estimatedDuration;
+        }
+
+        public void setEstimatedDuration(int estimatedDuration) throws Exception {
+    	this.estimatedDuration = estimatedDuration;
+        }
+
+        public double getAcceptableDeviation() {
+    	return acceptableDeviation;
+        }
+
+        public void setAcceptableDeviation(double acceptableDeviation) throws Exception {
+    	this.acceptableDeviation = acceptableDeviation;
+        }
+
+        public List<Task> getDependencyTasks() {
+    	return dependencyTasks;
+        }
+
+        public void setDependencyTasks(List<Task> dependencyTasks) throws Exception {
+    	this.dependencyTasks = dependencyTasks;
+        }
+
+        public void addDependencyTask(Task dependencyTask) throws Exception {
+    	this.dependencyTasks.add(dependencyTask);
+        }
+
+        public Task getAlternateTask() {
+    	return alternateTask;
+        }
+        
+        public void setAlternateTask(Task alternateTask) throws Exception {
+    	this.alternateTask = alternateTask;
+        }
 }
--- TaskMan/src/data/TaskUpdateData.java
+++ TaskMan/src/data/TaskUpdateData.java
@@ -1,5 +1,35 @@
 package data;
 
-public class TaskUpdateData {
+import java.util.Date;
 
+import task.Status;
+
+public class TaskUpdateData {
+	private Date startTime;
+	private Date endTime;
+	private Status status;
+	
+	public Date getStartTime(){
+		return startTime;
+	}
+	
+	public void setStartTime(Date startTime){
+		this.startTime = startTime;
+	}
+	
+	public Date getEndTime(){
+		return endTime;
+	}
+	
+	public void setEndTime(Date endTime){
+		this.endTime = endTime;
+	}
+	
+	public Status getStatus(){
+		return status;
+	}
+	
+	public void setStatus(Status status){
+		this.status = status;
+	}
 }
