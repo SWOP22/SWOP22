12dbeadef62d738be9f9cfab617e69dfb663db18
MichaelDohou <dohou.michael@gmail.com>
2015-03-10 17:45:41 +0100

    Added missing methods, placed all exception in InvalidProjectDataException and removed Test Console

--- TaskMan/src/data/InvalidProjectDataException.java
+++ TaskMan/src/data/InvalidProjectDataException.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.Date;
+
 public class InvalidProjectDataException extends Exception {
 
 	/**
@@ -9,5 +11,26 @@ public class InvalidProjectDataException extends Exception {
 	
 	public String getMessage(){
 		return "The information in the ProjectData object is not valid";
+	}
+	
+	//Field Checks
+	public void checkName(String name) throws InvalidProjectDataException {
+		if(name.equals(""))
+			throw new InvalidProjectDataException();
+	}
+	
+	public void checkDescription(String description) throws InvalidProjectDataException {
+		if(description.equals(""))
+			throw new InvalidProjectDataException();
+	}
+	
+	public void checkCreationTime(Date creationTime) throws InvalidProjectDataException {
+		if(creationTime == null)
+			throw new InvalidProjectDataException();
+	}
+	
+	public void checkDueTime(Date dueTime) throws InvalidProjectDataException {
+		if(dueTime == null)
+			throw new InvalidProjectDataException();
 	}
 }
